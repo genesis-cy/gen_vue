e401a71fee29bbc2dd028696f2767a0edc5e8bc5
Asher Raziel <gmon01@gmail.com>
2023-03-18 19:36:24 +0200

    genlife

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <img class="logo" src="@/assets/genIcon.png" />
         <q-space />
         <q-tabs v-model="tab" class="text-teal">
-          <q-tab name="two" @click="tab = 'two'" label="GenLife Card" />
+          <q-tab name="two" @click="tab = 'two'" label="Gen Card" />
           <q-tab name="one" @click="tab = 'one'" label="Trials" />
           <q-tab name="three" @click="tab = 'three'" label="Clinic" />
         </q-tabs>
